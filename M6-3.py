be91e7e3554ed00fc3ed60dcdf2bb88a323179e6
Dirom22 <100908070+Dirom22@users.noreply.github.com>
2022-12-07 18:55:38 +0300

    Add files via upload

--- M6-3.py
+++ M6-3.py
@@ -2,25 +2,22 @@ import requests
 from datetime import datetime
 from threading import Thread
 
-a = ['http://github.ru', 'http://yahoo.com/post', 'http://1plitka-spb.ru//post' , 'http://google.ru/post', 'http://ru.wikipedia.org/post']
+a = ['http://github.ru', 'http://yahoo.com/post', 'http://1plitka-spb.ru//post', 'http://google.ru/post', 'http://ru.wikipedia.org/post']
 
 def get_html(link):
-	for i in range(len(a)):
-		link = requests.get(a[i])
-		print(link.url)
+	response = requests.get(a[i])
+	print(link, len(response.text))
 	
-
 t1 = datetime.now()
 
-for i in range(len(a)):
-	response = requests.get(a[i])
-	print(response.url)
+for i in range(5):
+	print(get_html(a[i]))
 
 print('time ', 'Секунд: ', (datetime.now() - t1).seconds, 'Микросекунд: ', (datetime.now() - t1).microseconds)
 
-threads = [Thread(target = get_html, args=(i +1, ))]
+threads = [Thread(target = get_html, args = [a [i]]) for i in range(5)]
 
-t2 = datetime.now()
+t2 = datetime.now() 
 
 for t in threads:
 	t.start()
